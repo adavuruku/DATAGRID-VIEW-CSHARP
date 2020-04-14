cba8a581b54e66ec6c27784258da5ede03442ad2
Abdulraheem Sherif Adavuruku <abdulraheem.sherif@outsourceglobal.com>
2020-04-14 20:58:10 +0100

    COMPLETED

---
+++ encryption_and_decryption/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace encryption_and_decryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENCRYPT AND SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DECRYPT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(388, 128);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(164, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(164, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(388, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "User";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(159, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 379);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ encryption_and_decryption/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace encryption_and_decryption
+{
+    public partial class Form1 : Form
+    {
+        string inputFile="", outputFile ="";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               // inputFile = @"C:\Users\sherif146\Desktop\sherif.jpg";
+               // outputFile = @"C:\Users\sherif146\Desktop\sherif_enc.jpg";
+                
+                string password = @"myKey123"; // Your Key Here
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+                
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+              //  RijndaelManaged RMCrypto = new RijndaelManaged();
+                TripleDESCryptoServiceProvider RMCrypto = new TripleDESCryptoServiceProvider();
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+                FileStream fsIn = new FileStream(outputFile, FileMode.Create);
+                FileStream fsIn = new FileStream(outputFile, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();iiiiiiiiiiiiiiiiiiii
+            }
+            catch
+            {
+                MessageBox.Show("Encryption failed!", "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string password = @"myKey123"; // Your Key Here
+          //  inputFile = @"C:\Users\sherif146\Desktop\sherif_enc.jpg";
+           // outputFile = @"C:\Users\sherif146\Desktop\sherif_decrypt.jpg";
+
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] key = UE.GetBytes(password);
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+         //   RijndaelManaged RMCrypto = new RijndaelManaged();
+            TripleDESCryptoServiceProvider RMCrypto = new TripleDESCryptoServiceProvider();
+            CryptoStream cs = new CryptoStream(fsCrypt,
+                RMCrypto.CreateDecryptor(key, key),
+                CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int data;
+            while ((data = cs.ReadByte()) != -1)
+                fsOut.WriteByte((byte)data);
+
+            fsOut.Close();
+            cs.Close();
+            fsCrypt.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+          //  openFileDialog1.Filter = "Image Files (*.*);
+            string PICS = String.Empty;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+                //button3.Enabled = false;
+                string judee = "";
+                judee = openFileDialog1.FileName;
+                FileInfo file1 = new FileInfo(judee);
+                //MessageBox.Show(judee);
+                inputFile = judee;
+                
+                //file1.Name  file1.Extension file1.exist
+                string file_name = file1.Name;
+                outputFile = @"C:\Users\sherif146\Desktop\Encrypt\" + file_name;
+                //string file_extension = file1.Extension;
+                //string new_file = file_name + file_extension;
+                label1.Text = file_name;
+                
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            //  openFileDialog1.Filter = "Image Files (*.*);
+            string PICS = String.Empty;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+                //button3.Enabled = false;
+                string judee = "";
+                judee = openFileDialog1.FileName;
+                FileInfo file1 = new FileInfo(judee);
+                //MessageBox.Show(judee);
+                inputFile = judee;
+
+                //file1.Name  file1.Extension file1.exist
+                string file_name = file1.Name;
+                outputFile = @"C:\Users\sherif146\Desktop\Decrypt\" + file_name;
+                //string file_extension = file1.Extension;
+                //string new_file = file_name + file_extension;
+                label4.Text = file_name;
+
+            }
+
+        }
+    }
+}
